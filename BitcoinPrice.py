b160d62e9aa664c2f4160b65b08b7675a8090649
/The Reron <67793794+TheRennn@users.noreply.github.com>
2024-04-25 21:00:23 -0300

    Update BitcoinPrice.py

--- BitcoinPrice.py
+++ BitcoinPrice.py
@@ -9,4 +9,4 @@ if response.status_code == 200:
     btc_price = data['USD']
     print("Bitcoin Price:", btc_price)
 else:
-    print("Erro ao fazer solicitação à API do CryptoCompare:", response.text)
+    print("Error!", response.text)
